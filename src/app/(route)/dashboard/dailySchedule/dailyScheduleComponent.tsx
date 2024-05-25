b9d1a3fda80cbb8ec03f6eeea7868d7970ed0c80
seulbiLee <drizzle8926@gmail.com>
2024-05-25 10:56:42 +0900

    modified detail schedule

--- src/app/(route)/dashboard/dailySchedule/dailyScheduleComponent.tsx
+++ src/app/(route)/dashboard/dailySchedule/dailyScheduleComponent.tsx
@@ -1,107 +1,86 @@
 "use client";
 
-import { UnstyledButton, Button, Select } from "@mantine/core";
+import { UnstyledButton, Button, Select, ActionIcon, rem } from "@mantine/core";
+import { TimeInput } from "@mantine/dates";
 import styles from "@/app/(route)/dashboard/dashboard.module.scss";
 import { useRouter, useSearchParams } from "next/navigation";
-import { IconX } from "@tabler/icons-react";
-import { FC, PropsWithChildren } from "react";
+import { IconClock, IconX } from "@tabler/icons-react";
+import { FC, PropsWithChildren, useRef } from "react";
+import "@mantine/dates/styles.css";
+import { useForm } from "@mantine/form";
 
-const DailyScheduleComponent: FC<PropsWithChildren> = ({
-  
-}: {
-
-}) => {
+const DailyScheduleComponent: FC<PropsWithChildren> = ({}: {}) => {
+  const startTimeRef = useRef<HTMLInputElement>(null);
+  const endTimeRef = useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
   const route = useRouter();
 
   const date = searchParams.get("date");
 
-  const hours = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-  const minutes = ["00","05","10","15","20","25","30","35","40","45","50","55"]
-  const ampm = ["am","pm"]
+  const startTimePicker = (
+    <ActionIcon
+      variant="subtle"
+      color="gray"
+      onClick={() => startTimeRef.current?.showPicker()}
+    >
+      <IconClock style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+    </ActionIcon>
+  );
+
+  const endTimePicker = (
+    <ActionIcon
+      variant="subtle"
+      color="gray"
+      onClick={() => endTimeRef.current?.showPicker()}
+    >
+      <IconClock style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+    </ActionIcon>
+  );
 
   return (
     <>
-      <div className={styles.container}>
-        <div className="container-inner">
-          <div className="detail-title">
-            <div>{date}</div>
-            <UnstyledButton
-              type="button"
-              onClick={() => route.push("/dashboard")}
-              variant="default"
-              size="xs"
-            >
-              <IconX stroke={2} />
-            </UnstyledButton>
-          </div>
-          <form>
-            <Select
-              placeholder="Select Store"
-              data={["store1", "store2"]}
-              defaultValue="React"
-              allowDeselect={false}
-            />
-            <Select
-              placeholder="Select Employee"
-              data={["p1", "p2", "p3", "p4"]}
-              defaultValue="React"
-              allowDeselect={false}
-              mt="md"
-            />
-            <div className="select-time">
-              <Select
-                label="Start"
-                placeholder="hour"
-                data={hours}
-                defaultValue="React"
-                allowDeselect={false}
-                mt="sm"
-              />
-              <Select
-                placeholder="minute"
-                data={minutes}
-                defaultValue="React"
-                allowDeselect={false}
-                mt="sm"
-              />
-              <Select
-                placeholder="am/pm"
-                data={ampm}
-                defaultValue="React"
-                allowDeselect={false}
-                mt="sm"
-              />            
-            </div>
-            <div className="select-time">
-              <Select
-                label="End"
-                placeholder="hour"
-                data={hours}
-                defaultValue="React"
-                allowDeselect={false}
-                mt="sm"
-              />
-              <Select
-                placeholder="minute"
-                data={minutes}
-                defaultValue="React"
-                allowDeselect={false}
-                mt="sm"
-              />
-              <Select
-                placeholder="am/pm"
-                data={ampm}
-                defaultValue="React"
-                allowDeselect={false}
-                mt="sm"
-              />            
-            </div>
-            <Button mt="md" fullWidth>
-              + add
-            </Button>
-          </form>
+      <div className="container-inner">
+        <div className="detail-title">
+          <div>{date}</div>
+          <UnstyledButton
+            type="button"
+            onClick={() => route.back()}
+            variant="default"
+            size="xs"
+          >
+            <IconX stroke={2} />
+          </UnstyledButton>
         </div>
+        <form>
+          <Select
+            placeholder="Select Store"
+            data={["store1", "store2"]}
+            defaultValue="store1"
+            allowDeselect={false}
+          />
+          <Select
+            placeholder="Select Employee"
+            data={["p1", "p2", "p3", "p4"]}
+            defaultValue="p1"
+            allowDeselect={false}
+            mt="md"
+          />
+          <TimeInput
+            label="Start"
+            ref={startTimeRef}
+            rightSection={startTimePicker}
+            mt="sm"
+          />
+          <TimeInput
+            label="End"
+            ref={endTimeRef}
+            rightSection={endTimePicker}
+            mt="sm"
+          />
+          <Button mt="md" fullWidth>
+            + add
+          </Button>
+        </form>
       </div>
     </>
   );
