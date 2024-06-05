35e82a9f7c9e3669244b15e5178c66125d2b7514
seulbiLee <drizzle8926@gmail.com>
2024-06-06 07:34:42 +0900

    add function - amount daily amounts

--- src/app/api/setDailySchedule/route.ts
+++ src/app/api/setDailySchedule/route.ts
@@ -3,9 +3,30 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
-  const supabaseAdmin = createAdmin();
 
-  console.log(data);
+  const supabaseAdmin = createAdmin();
+  const userId = data.memberId;
+
+  const { data: userData, error: userError } = await supabaseAdmin
+    .from("store_members")
+    .select("hourly_rate")
+    .eq("user_id", userId)
+    .eq("store_id", data.storeId)
+    .single();
+
+  if (userError) {
+    return NextResponse.json(userError, { status: 400 });
+  }
+
+  const startTime =
+    data.startTime.slice(0, 2) * 60 + data.startTime.slice(2, 2);
+  const endTime = data.endTime.slice(0, 2) * 60 + data.startTime.slice(2, 2);
+  const payPerMin = userData.hourly_rate ? userData.hourly_rate / 60 : null;
+  const amounts = payPerMin
+    ? ((endTime - startTime) * payPerMin).toFixed(2)
+    : null;
+
+  console.log(startTime, endTime, payPerMin, amounts);
 
   const { error: insertError } = await supabaseAdmin.from("schedules").insert({
     store_id: data.storeId,
@@ -13,10 +34,12 @@ export async function POST(req: NextRequest) {
     date: data.date,
     start: data.startTime,
     end: data.endTime,
+    amounts: amounts,
   });
 
   if (insertError) {
-    return NextResponse.json(insertError, {status:400});
+    console.log(insertError);
+    return NextResponse.json(insertError, { status: 400 });
   }
 
   return NextResponse.json("success");
